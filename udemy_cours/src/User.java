f95b4b8f4ef49bc5a7e5602ec939fee6099e3c01
Mehmet Elibeyli <tomristt9@gmail.com>
2024-02-08 12:54:19 +0400

    add AuthenticationException class for handling authentication errors

--- udemy_cours/src/User.java
+++ udemy_cours/src/User.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class User {
+public class User {
+    private String name;
+    private String username;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public User(String name, String username) {
+        this.name = name;
+        this.username = username;
+    }
+
+    @Override
+    public String toString() {
+        return "User:" +
+                "name='" + name + '\'' +
+                ", username='" + username + '\'' ;
+    }
 }
