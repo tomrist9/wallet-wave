72d4df0ca33bf6bb701efb039b9307ad226bcbd8
Mehmet Elibeyli <tomristt9@gmail.com>
2024-05-07 16:41:29 +0400

    add task endpoints and spring security

--- file_writing/src/main/java/org/example/FileWriterExample.java
+++ file_writing/src/main/java/org/example/FileWriterExample.java
@@ -1,21 +1,45 @@
 package org.example;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.List;
 
 public class FileWriterExample {
-    public static void main(String[] args) {
-        String fileName = "nazgozeli";
+    public static void foo() throws Exception {
+        Class.forName("com.mysql.cj.jdbc.Driver");
 
+        String url="jdbc:mysql://localhost:3306";
+        String username="root";
+        String password="postgres";
+        Connection connection= DriverManager.getConnection(url, username, password);
+        Statement stmt= connection.createStatement();
+        stmt.execute("select * from user");
+        ResultSet resultSet= stmt.getResultSet();
 
-        try {
-            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName));
-            bufferedWriter.write("ask boceyi");
-            bufferedWriter.close();
-            System.out.println("ask bocuvgu");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        while(resultSet.next()){
+            int id=resultSet.getInt("id");
+            String name=resultSet.getString("name");
+            String surname=resultSet.getString("surname");
+            String phone=resultSet.getString("phone");
+            String email=resultSet.getString("email");
+            System.out.println("id"+id);
+            System.out.println("name"+name);
+            System.out.println("surname"+surname);
+            System.out.println("phone"+phone);
+            System.out.println("email"+email);
+
         }
+        System.out.println();
+    }
+
+    public static void main(String[] args) throws Exception {
+        foo();
     }
 }
