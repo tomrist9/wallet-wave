602726b4c519faad4ef65eb74397bf9a7a8cba5c
Tomris Teymurlu <tomristt9@gmail.com>
2024-10-28 14:18:28 +0400

    Delete ResumeDbAppJPASpring/ResumeDbAppJPASpring/src/main/java/com/example/ResumeDbAppJPASpring directory

--- ResumeDbAppJPASpring/ResumeDbAppJPASpring/src/main/java/com/example/ResumeDbAppJPASpring/dao/inter/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ResumeDbAppJPASpring.dao.inter;
-
-import com.example.ResumeDbAppJPASpring.entity.User;
-import jakarta.persistence.criteria.CriteriaBuilder;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-//    User findByName
-}
