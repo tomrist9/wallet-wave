d09002df64a4057f74d8a02caa53a05b1c3c8db7
Tomris Teymurlu <tomristt9@gmail.com>
2024-10-28 14:21:27 +0400

    Delete user_management directory

--- user_management/src/main/java/com/example/user_management/registration/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.user_management.registration.repository;
-
-import com.example.user_management.registration.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
