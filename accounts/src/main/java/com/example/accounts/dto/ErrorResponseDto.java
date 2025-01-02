650f5fb717d61516c37355ecf7b53e9bcd9006ef
Tomris Teymurlu <tomristt9@gmail.com>
2025-01-02 12:34:55 +0400

    added open api documentation dependency

--- accounts/src/main/java/com/example/accounts/dto/ErrorResponseDto.java
+++ accounts/src/main/java/com/example/accounts/dto/ErrorResponseDto.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 import java.time.LocalDateTime;
 
 @Data
-@AllArgsConstructor
+
 public class ErrorResponseDto {
     private String apiPath;
     private HttpStatus errorCode;
