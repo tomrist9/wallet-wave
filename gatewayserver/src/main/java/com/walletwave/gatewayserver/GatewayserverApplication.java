e160bbe51bb44fbc3bd33b4558c764e184d02971
Tomris Teymurlu <tomristt9@gmail.com>
2025-02-12 23:35:38 +0400

    added routing configuration to the gatewayserver

--- gatewayserver/src/main/java/com/walletwave/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/walletwave/gatewayserver/GatewayserverApplication.java
@@ -2,12 +2,35 @@ package com.walletwave.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+import static ch.qos.logback.classic.spi.ThrowableProxyVO.build;
 
 @SpringBootApplication
 public class GatewayserverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayserverApplication.class, args);
+	}
+	@Bean
+	public RouteLocator walletWaveRouteConfig(RouteLocatorBuilder routeLocatorBuilder){
+		return routeLocatorBuilder
+                .routes()
+                .route(p-> p
+						.path("/walletwave/accounts/**")
+                        .filters(f -> f.rewritePath("/walletwave/accounts/(?<segment>.*)","/$[segment}"))
+                        .uri("lb://ACCOUNTS"))
+				.route(p-> p
+				.path("/walletwave/loans/**")
+				.filters(f -> f.rewritePath("/walletwave/loans/(?<segment>.*)","/$[segment}"))
+				.uri("lb://LOANS"))
+				.route(p-> p
+						.path("/walletwave/cards/**")
+						.filters(f -> f.rewritePath("/walletwave/cards/(?<segment>.*)","/$[segment}"))
+						.uri("lb://CARDS"))
+                .build();
 	}
 
 }
