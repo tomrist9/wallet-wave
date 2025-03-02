9fa0c791c8edfe0ef1595ca43c529523a58fd3cb
Tomris Teymurlu <tomristt9@gmail.com>
2025-03-02 15:28:50 +0400

    implemented retry pattern in accounts

--- accounts/src/main/java/com/example/controller/AccountsController.java
+++ accounts/src/main/java/com/example/controller/AccountsController.java
@@ -7,6 +7,7 @@ import com.example.dto.CustomerDTO;
 import com.example.dto.ErrorResponseDto;
 import com.example.dto.ResponseDTO;
 import com.example.service.IAccountsService;
+import io.github.resilience4j.retry.annotation.Retry;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -15,6 +16,8 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -34,7 +37,7 @@ import org.springframework.web.bind.annotation.*;
 @Validated
 public class AccountsController {
 
-
+    private static final Logger logger = LoggerFactory.getLogger(AccountsController.class);
     private final IAccountsService iAccountsService;
 
     @Value("${build.version}")
@@ -164,11 +167,20 @@ public class AccountsController {
             )
     }
     )
+    @Retry(name="getBuildInfo", fallbackMethod = "getBuildInfoFallback")
     @GetMapping("/build-info")
     public ResponseEntity<String> getBuildInfo(){
+        logger.debug("getBuildInfo() method Invoked");
+        throw new NullPointerException();
+//        return ResponseEntity
+//                .status(HttpStatus.OK)
+//                .body(buildVersion);
+    }
+    public ResponseEntity<String> getBuildInfoFallback(Throwable throwable) {
+        logger.debug("getBuildInfoFallback() method Invoked");
         return ResponseEntity
                 .status(HttpStatus.OK)
-                .body(buildVersion);
+                .body("0.9");
     }
 
     @Operation(
