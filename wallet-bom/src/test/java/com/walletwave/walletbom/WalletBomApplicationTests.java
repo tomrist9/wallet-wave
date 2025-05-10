7366159972c61e50ff762ba8376688efe76fc3d6
tomrist9 <tomristt9@gmail.com>
2025-05-10 13:53:45 +0400

    optimizing microservices

---
+++ wallet-bom/src/test/java/com/walletwave/walletbom/WalletBomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walletwave.walletbom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletBomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
